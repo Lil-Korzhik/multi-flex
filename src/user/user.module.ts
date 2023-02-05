58b2e42bb3791c36eed38cbc4f3b625d0db3e688
Lil-Korzhik <mr.purple2007@gmail.com>
2023-02-05 15:50:50 +0100

    Fix database on production

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,23 @@
-import { Module } from '@nestjs/common';
-import { UserController } from './user.controller';
-import { UserService } from './user.service';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { TypegooseModule } from 'nestjs-typegoose'
+import { UserController } from './user.controller'
+import { UserModel } from './user.model'
+import { UserService } from './user.service'
 
 @Module({
-  controllers: [UserController],
-  providers: [UserService]
+	imports: [
+		TypegooseModule.forFeature([
+			{
+				typegooseClass: UserModel,
+				schemaOptions: {
+					collection: 'User'
+				}
+			}
+		]),
+		ConfigModule
+	],
+	controllers: [UserController],
+	providers: [UserService]
 })
 export class UserModule {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { ModelType } from '@typegoose/typegoose/lib/types'
+import { InjectModel } from 'nestjs-typegoose'
+import { UserModel } from './user.model'
 
 @Injectable()
-export class UserService {}
+export class UserService {
+	constructor(
+		@InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>
+	) {}
+
+	async getById() {
+		return { email: 'address@gmail.com' }
+	}
+}
